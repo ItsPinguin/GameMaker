0e90ca3c5953a1e39d7726b0ab3df2565b80175d
ItsPinguin <martinmcnam@gmail.com>
2026-05-02 18:33:00 +0200

    refactor!(commands): simplify GameMakerCommand by removing unused logic and adjusting item handling

--- src/main/kotlin/fr/ping/gamemaker/commands/GameMakerCommand.kt
+++ src/main/kotlin/fr/ping/gamemaker/commands/GameMakerCommand.kt
@@ -1,10 +1,8 @@
 package fr.ping.gamemaker.commands
 
 import fr.ping.gamemaker.GameMakerPlugin.Companion.itemRegistry
-import fr.ping.gamemaker.addons.AddonManager
 import fr.ping.gamemaker.builtin.resource.I18n
 import fr.ping.gamemaker.items.ItemManager
-import fr.ping.gamemaker.items.templates.models.ItemTemplate
 import fr.ping.utils.resources.ResourceManager
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
@@ -22,11 +20,11 @@ object GameMakerCommand : TabExecutor {
   ): List<String?>? {
     if ((args.size) > 1) {
       when (args.getOrNull(0) ?: "") {
-        "give" -> return listOf(*ResourceManager.getRegistry("items")?.listIds()?.toTypedArray() ?: arrayOf())
+        "give" -> return listOf(*itemRegistry.listIds().toTypedArray())
       }
       return null
     }
-    return listOf("help", "reloadall", "giveall", "saveall", "reload", "translate", "give")
+    return listOf("help", "giveall", "reload", "translate", "give")
   }
 
   override fun onCommand(
@@ -36,81 +34,35 @@ object GameMakerCommand : TabExecutor {
     args: Array<out String>
   ): Boolean {
     when (args.getOrNull(0) ?: "help") {
-      "reload", "reloadall" -> {
-        sender.sendMessage("Reloading...")
+      "reload" -> {
+        sender.sendMessage("§fReloading...")
         measureTime {
           ResourceManager.loadAllResources(true, true)
-          AddonManager.reload()
         }.let {
-          sender.sendMessage("Done reloading. Took ${it.inWholeMilliseconds} ms")
-        }
-      }
-      "saveall" -> {
-        sender.sendMessage("Saving...")
-        measureTime {
-          AddonManager.save()
-        }.let {
-          sender.sendMessage("Done saving. Took ${it.inWholeMilliseconds} ms")
+          sender.sendMessage("§7Done reloading. Took §f${it.inWholeMilliseconds} ms")
         }
       }
       "giveall" -> {
         if (sender !is Player) return true
         itemRegistry.listResources().forEach {
           sender.inventory.addItem(ItemManager.buildItem(it))
-          sender.sendMessage("Gave ${it.id}")
         }
+        sender.sendMessage("§7Gave §f${itemRegistry.listResources().size} items")
       }
       "give" -> {
         if (sender !is Player) return true
         val item = args.getOrNull(1) ?: run {
-          sender.sendMessage("Usage: /gamemaker give <item>")
+          sender.sendMessage("§7Usage: §f/gamemaker give <item>")
           return true
         }
-        ItemManager.buildItem(
-          ResourceManager[item, ItemTemplate::class.java]?.resource
-        ).let {
-          sender.inventory.addItem(it)
-          sender.sendMessage("Gave $item")
-        }
+        sender.inventory.addItem(ItemManager.buildItem(item))
+        sender.sendMessage("§7Gave §f$item")
       }
-      //"reload" -> {
-      //  val index = args?.getOrNull(1) ?: run {
-      //    sender.sendMessage("Usage: /gamemaker reload <resource>")
-      //    return true
-      //  }
-      //  val registry = index.split("/").firstOrNull() ?: return true
-      //  val resource = index.removePrefix("$registry/")
-//
-        //when (registry.split("/").firstOrNull() ?: "") {
-        //  "item", "items" -> {
-        //    ResourceIO.loadToRegistry(gmkFolder.resolve("items"), gmkFolder.resolve("items").resolve("$resource.json"), gmk.useRegistry<ItemTemplate>("items"))
-        //    sender.sendMessage("Reloaded $resource")
-        //    return true
-        //  }
-        //  "lang" -> {
-        //    ResourceIO.loadToRegistry(gmkFolder.resolve("lang"), gmkFolder.resolve("lang").resolve("$resource.json"), gmk.useRegistry<I18n>("lang"))
-        //    sender.sendMessage("Reloaded $resource")
-        //    return true
-        //  }
-        //  "action", "actions" -> {
-        //    ResourceIO.loadToRegistry(gmkFolder.resolve("actions"), gmkFolder.resolve("actions").resolve("$resource.json"), gmk.useRegistry<Action>("actions"))
-        //    sender.sendMessage("Reloaded $resource")
-        //    return true
-        //  }
-        //  "trigger", "triggers" -> {
-        //    ResourceIO.loadToRegistry(gmkFolder.resolve("triggers"), gmkFolder.resolve("triggers").resolve("$resource.json"), gmk.useRegistry<Trigger>("triggers"))
-        //    AddonManager.TriggerManager.reloadTrigger(resource)
-        //    sender.sendMessage("Reloaded $resource")
-        //    return true
-        //  }
-        //}
-
-      //}
       "translate" -> {
         sender.sendMessage(i18n?.resource?.translateAndInsert("some.key.to.something", mapOf("thing" to System.currentTimeMillis().toString())).toString())
       }
       else -> {
-        sender.sendMessage("reload, save")
+        sender.sendMessage("reload")
       }
     }
     return true
