d56cdb88b5b7d2d344b759a2ad93a3ed508a3a18
ItsPinguin <martinmcnam@gmail.com>
2026-06-19 08:43:28 +0200

    feat(resource): enhance `MapResource` serialization/deserialization with `SerializedName` support and data parsing

--- src/main/kotlin/fr/ping/gamemaker/resource/MapResource.kt
+++ src/main/kotlin/fr/ping/gamemaker/resource/MapResource.kt
@@ -2,11 +2,13 @@ package fr.ping.gamemaker.resource
 
 import com.google.gson.TypeAdapter
 import com.google.gson.annotations.JsonAdapter
+import com.google.gson.annotations.SerializedName
 import com.google.gson.stream.JsonReader
 import com.google.gson.stream.JsonToken
 import com.google.gson.stream.JsonWriter
 import fr.ping.gamemaker.GameMakerPlugin
 import fr.ping.utils.resources.Resource
+import fr.ping.utils.resources.ResourceManager
 
 @JsonAdapter(MapResource.Adapter::class)
 open class MapResource(
@@ -21,7 +23,14 @@ open class MapResource(
 
   open class GeneralAdapter <T : MapResource> (val clazz: Class<T>) : TypeAdapter<T>() {
     override fun write(out: JsonWriter?, value: T?) {
-      out?.jsonValue(GameMakerPlugin.Companion.gson.toJson(value?.data))
+      clazz.declaredFields.forEach { field ->
+        val names = field.getDeclaredAnnotation(SerializedName::class.java)?.let {
+          mutableListOf(it.value).apply { addAll(it.alternate) }
+        }
+        val chosenName = names?.firstOrNull { value?.data[it] != null } ?: names?.firstOrNull() ?: field.name
+        value?.data[chosenName] = field.get(value)
+      }
+      out?.jsonValue(GameMakerPlugin.gson.toJson(value?.data))
     }
 
     override fun read(`in`: JsonReader?): T? {
@@ -31,6 +40,14 @@ open class MapResource(
         mapResource.data = GameMakerPlugin.gson.fromJson(`in`, MutableMap::class.java)
         mapResource.id = mapResource.data["id"] as? String ?: mapResource.id
         mapResource.type = mapResource.data["type"] as? String ?: mapResource.type
+        clazz.declaredFields.forEach { field ->
+          field.isAccessible = true
+          val names : List<String> = field.getDeclaredAnnotation(SerializedName::class.java)?.let {
+            mutableListOf(it.value).apply { addAll(it.alternate) }
+          } ?: listOf()
+          val value = names.firstNotNullOfOrNull { mapResource.data[it] } ?: mapResource.data[field.name] ?: return@forEach
+          field.set(mapResource, ResourceManager.parseAny(value, field.type))
+        }
         return mapResource
       }
       return clazz.newInstance()
